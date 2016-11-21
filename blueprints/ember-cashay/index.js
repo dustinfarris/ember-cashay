646b61b1bd51912eeb3582d6486bcabe823cd43e
Dustin Farris <dustin.farris@gmail.com>
2016-11-21 11:41:37 -0500

    Drop babel-runtime
    Dependency was removed upstream.

--- blueprints/ember-cashay/index.js
+++ blueprints/ember-cashay/index.js
@@ -8,13 +8,11 @@ module.exports = {
     return this.addAddonsToProject({
       packages: [
         { name: 'ember-browserify', target: '^1.1.13' },
-        { name: 'ember-redux', target: '^1.6.0' }
+        { name: 'ember-redux', target: '^1.9.0' }
       ]
     }).then(function() {
       return this.addPackagesToProject([
-        // TODO: Remove babel-runtime from host if possible
-        { name: 'babel-runtime', target: '^6.18.0' },
-        { name: 'cashay', target: '^0.20.12' },
+        { name: 'cashay', target: '^0.22.0' },
         { name: 'graphql', target: '^0.7.1' }
       ])
     }.bind(this))
